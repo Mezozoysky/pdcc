29ec98ae1d90cc2f535ca78d4823a3380a64bca6
Stanislav Demyanovich <s.demyanovich@protonmail.com>
2023-02-27 23:41:21 +0400

    Project stub

---
+++ sources/pdcc/main.cpp
@@ -0,0 +1,17 @@
+#include <fmt/format.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+int main(int argc, char ** argv)
+{
+    fmt::print("pdcc");
+
+    return 0;
+}
+
+#ifdef __cplusplus
+}
+#endif
